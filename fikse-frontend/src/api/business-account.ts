e2410f66f0ca32fb62b5887c4cbca5cf968e013d
Snorre Sæther <112980079+Snorre98@users.noreply.github.com>
2025-03-31 20:39:48 +0200

    setup tanstack query and starting point for http handling and api requests (#7)
    * setup query provider
    
    * adds skeleton for api function
    
    * adds constants for api

---
+++ fikse-frontend/src/api/business-account.ts
@@ -0,0 +1,14 @@
+import axios, { type AxiosResponse } from "axios";
+import { BACKEND_URL } from "./constants";
+
+
+interface createBusinessAccount {
+    // TODO: add correct fields
+    form_data: string;
+}
+
+export async function createBusinessAccount(data: createBusinessAccount): Promise<AxiosResponse> {
+    const url = `${BACKEND_URL}/create-busniness-account`;
+    const response = await axios.post(url, data, { withCredentials: true });
+    return response.data;
+}

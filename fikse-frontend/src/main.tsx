f77c7c0c8300be7eed6040acf96f2d3192552134
Snorre Sæther <snosaet@gmail.com>
2025-03-31 20:25:26 +0200

    setup query provider

--- fikse-frontend/src/main.tsx
+++ fikse-frontend/src/main.tsx
@@ -3,12 +3,36 @@ import './index.css'
 import { RouterProvider } from 'react-router';
 import ReactDOM from 'react-dom/client';
 import { router } from './Router/router.tsx';
+import { QueryCache, QueryClient, QueryClientProvider } from '@tanstack/react-query';
+import { AxiosError } from 'axios';
 
+const queryClient = new QueryClient({
+  queryCache: new QueryCache({
+    onError: (error) => {
+      console.error(error);
+    },
+  }),
+  defaultOptions: {
+    queries: {
+      retry: (failureCount, error) => {
+        // Don't retry on HTTP 404 responses
+        if (error instanceof AxiosError && error.response?.status === 404) {
+          return false;
+        }
+        // max 2 retries
+        return failureCount < 2;
+      },
+    },
+  },
+})
 
 const root = ReactDOM.createRoot(document.getElementById('root') as HTMLElement);
 
 root.render(
+  <QueryClientProvider client={queryClient}>
   <StrictMode>
     <RouterProvider router={router}/>
-  </StrictMode>,
+  </StrictMode>
+  </QueryClientProvider>
+,
 )

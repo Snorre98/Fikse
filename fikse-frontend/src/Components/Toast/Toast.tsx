8f7c129fabea1c63392fdb6749acfbb8015942c0
Snorre Sæther <snosaet@gmail.com>
2025-04-07 10:06:22 +0200

    uses error message

--- fikse-frontend/src/Components/Toast/Toast.tsx
+++ fikse-frontend/src/Components/Toast/Toast.tsx
@@ -1,26 +1,21 @@
-import { type ReactNode, useState } from "react";
+import type { ReactNode } from "react";
 import styles from "./Toast.module.scss";
 
 type ToastProps = {
 	title?: string;
 	description?: string;
-	onShow?: () => void;
 	children?: ReactNode;
 };
 
-export function Toast({ title="title", description="description desciondescion desption description", children, onShow }: ToastProps) {
-	const [display, setDisplay] = useState(true);
-
-
-
+export function Toast({ title="title", description="description desciondescion desption description", children }: ToastProps) {
 	return (
 		<div className={styles.toast_wrapper}>
-			{display && (
+			
 				<div className={styles.toast}>
 					<h4 className={styles.toast_title}>{title}</h4>
 					<p className={styles.toast_description}>{description}</p>
 				</div>
-			)}
+			
 			{children}
 		</div>
 	);
